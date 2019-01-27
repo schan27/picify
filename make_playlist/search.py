3b4fab382508004dde717ff49672df92ee6033a5
Matt Wiens <mwiens91@gmail.com>
2019-01-27 10:52:01 -0800

    Add curating songs by playlists matching terms

--- make_playlist/search.py
+++ make_playlist/search.py
@@ -34,3 +34,47 @@ def search(search_terms):
     # flask.session['songs'] = songs
     # print(flask.session.get('songs'))
     return songs
+
+
+def search_by_playlist(search_terms):
+    # Get Spotify token
+    access_token = flask.session.get("access_token")
+
+    # Magic numbers
+    search_terms_used = 5
+    playlists_per_keyword = 5
+    tracks_per_playlist = 5
+
+    songs = []
+
+    for term in search_terms[:search_terms_used]:
+        playlist_response = requests.get(
+            "https://api.spotify.com/v1/search",
+            params={"q": term, "type": "playlist", "limit": playlists_per_keyword},
+            headers={"Authorization": "Bearer " + access_token},
+        )
+
+        for playlist in playlist_response.json()["playlists"]["items"]:
+            tracks_href = playlist["tracks"]["href"]
+
+            tracks_response = requests.get(
+                tracks_href,
+                params={"limit": tracks_per_playlist},
+                headers={"Authorization": "Bearer " + access_token},
+            )
+
+            for track_wrapper in tracks_response.json()["items"]:
+                track = track_wrapper["track"]
+
+                artists = [artist["name"] for artist in track["artists"]]
+                if len(artists) == 1:
+                    artists = artists[0]
+                elif len(artists) == 2:
+                    artists = " & ".join(artists)
+                else:
+                    artists = ", ".join(artists[:-1]) + " & " + artists[-1]
+
+                song_details = dict(id=track["id"], name=track["name"], artists=artists)
+                songs.append(song_details)
+
+    return songs
