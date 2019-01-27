9bb8855e793c038b7d42dcdb6269553698503951
Sophia Chan <ceasings@gmail.com>
2019-01-26 18:59:03 -0800

    added parse_image to flask app

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,19 @@
 import flask
 import os
+import io
 from werkzeug.utils import secure_filename
+from make_playlist import image_parse
+from google.oauth2 import service_account
 
+APP_BASE = os.path.dirname(os.path.realpath(__file__))
 
-UPLOAD_FOLDER = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "images"
-)
+UPLOAD_FOLDER = os.path.join(APP_BASE, "images")
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+credentials = service_account.Credentials.from_service_account_file(
+    os.path.join(os.path.dirname(APP_BASE), "grp_credentials.json")
+)
 
 app = flask.Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -34,9 +39,18 @@ def upload_file(name=None):
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return flask.redirect(flask.url_for('uploaded_file',
+            return flask.redirect(flask.url_for('parse_image',
                                     filename=filename))
     return flask.render_template('index.html', name=name)
+
+
+@app.route('/parse/<filename>')
+def parse_image(filename):
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    with io.open(image_path, "rb") as image_file:
+        raw_image = image_file.read()
+    result = image_parse.parse_image(raw_image, credentials)
+    return " ".join(result["labels"])
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
